7b72125cdf0e9d20daf16bc946bc358a87f48df6
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-04-02 03:00:17 -0300

    Upload

--- scripts/usage.js
+++ scripts/usage.js
@@ -7,7 +7,7 @@ async function generatePix() {
         additionalInfo: document.getElementById("additionalInfo").value
     };
 
-    const response = await fetch("http://localhost:3333/qrcode", {
+    const response = await fetch("apipixqrcode-production.up.railway.app/qrcode", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(pixData)
